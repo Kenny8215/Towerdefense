4d85f0ee9b8ca04a4dc098f47c42ab67e07cb3fd
WamZlay <wamzlay0@gmail.com>
2015-10-21 19:14:30 +0200

    CreateGrid

--- Towerdefense/Towerdefense/src/GameManager.cs
+++ Towerdefense/Towerdefense/src/GameManager.cs
@@ -9,5 +9,23 @@ namespace Towerdefense.src
 {
     class GameManager
     {
+        public List<Vector2> createGrid(int maxHeight, int amountOfFields){
+            List<Vector2> fieldPosition = new List<Vector2>();
+           int offset= maxHeight/amountOfFields;
+
+           for (int i = 0; i <= amountOfFields;i++ )
+           {
+               for (int j = 0; j <= amountOfFields; j++)
+               {
+                   fieldPosition.Add(new Vector2(offset / 2 * j, offset * (j + 1) - offset / 2));
+                    Console.WriteLine("X Position :" + fieldPosition.ElementAt(j + i).X);
+                    Console.WriteLine("Y Position :" + fieldPosition.ElementAt(j + i).Y);
+               }
+               }
+       
+                 return fieldPosition;
+        }
+
+
     }
 }
