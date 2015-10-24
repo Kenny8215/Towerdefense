44f48c287d6dc6efc96a085843ce406b02c01f16
WamZlay <wamzlay0@gmail.com>
2015-10-24 14:14:40 +0200

    Added DrawInitializedGrid Method - Initializes the Grid with a Texture2D object

--- Towerdefense/Towerdefense/src/GameManager.cs
+++ Towerdefense/Towerdefense/src/GameManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace Towerdefense.src
 {
@@ -18,21 +20,34 @@ namespace Towerdefense.src
             int y = 0;
             int x = 0;
 
-            for (int i = 0; i < amountOfFields; i++)
-            {
+            for (int i = 0; i < amountOfFields; i++){
                 y += offset / 2;
                 x = 0;
-                for (int j = 0; j < amountOfFields; j++)
-                {
-                   
+                for (int j = 0; j < amountOfFields; j++){
                     x += offset / 2;
                     positionArray[i, j] = new Vector2(x, y);
                     x += offset / 2;
                    //Console.WriteLine("X :" + positionArray[i, j].X + "Y :" + positionArray[i, j].Y);
                 }
+
                 y += offset / 2;
             }
             return positionArray;
+        }
+    
+        /*Draws the sprites at the center of each gridelement, can be used to initialize the grid with a specific texture,
+         * This method is called in the LoadContent method in the GameplayScreen*/
+        public void DrawInitializedGrid(Vector2[,] center, Texture2D texture, ContentManager content, SpriteBatch spriteBatch)
+        {
+            Vector2 textureCenter = new Vector2(texture.Width,texture.Height);
+
+            spriteBatch.Begin();
+            for(int i = 0; i<= center.GetLength(0); i++){
+                for(int j = 0; j<= center.GetLength(1); j++){
+                    spriteBatch.Draw(texture,center[i,j],null,Color.White,0,textureCenter,1F,SpriteEffects.None, 0 );                    
+                }
+                }
+            spriteBatch.End();
         }
 
 
@@ -96,13 +111,6 @@ namespace Towerdefense.src
         {
 
         }
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> e2917579dfe47bb0954eb275e3ca27ee91e278fe
     }
 
 }
