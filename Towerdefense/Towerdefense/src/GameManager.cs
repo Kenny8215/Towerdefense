167d1aec4451f219fa7872b312d3653fe04616e3
WamZlay <wamzlay0@gmail.com>
2015-10-24 11:25:48 +0200

    removed old creategrid method

--- Towerdefense/Towerdefense/src/GameManager.cs
+++ Towerdefense/Towerdefense/src/GameManager.cs
@@ -33,22 +33,6 @@ namespace Towerdefense.src
                 y += offset / 2;
             }
             return positionArray;
-
-        public List<Vector2> createGrid(int maxHeight, int amountOfFields){
-            List<Vector2> fieldPosition = new List<Vector2>();
-           int offset= maxHeight/amountOfFields;
-
-           for (int i = 0; i <= amountOfFields;i++ )
-           {
-               for (int j = 0; j <= amountOfFields; j++)
-               {
-                   fieldPosition.Add(new Vector2(offset / 2 * j, offset * (j + 1) - offset / 2));
-                    Console.WriteLine("X Position :" + fieldPosition.ElementAt(j + i).X);
-                    Console.WriteLine("Y Position :" + fieldPosition.ElementAt(j + i).Y);
-               }
-               }
-       
-                 return fieldPosition;
         }
 
 
