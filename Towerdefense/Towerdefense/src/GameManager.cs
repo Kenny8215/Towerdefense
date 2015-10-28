6f767c615acb9a5cb727cc49647c43d9ed3784cc
Robin <Ksekuchenbrot@gmx.de>
2015-10-28 17:54:48 +0100

    GameManager konstrucktor update

--- Towerdefense/Towerdefense/src/GameManager.cs
+++ Towerdefense/Towerdefense/src/GameManager.cs
@@ -11,8 +11,6 @@ namespace Towerdefense
 {
     class GameManager
     {
-        /*list of sprites*/
-        SortedList<enemyType, Texture2D> spriteList = new SortedList<enemyType, Texture2D>();
 
         /*list of tower in game*/
         List<Tower> towerList;
@@ -21,46 +19,16 @@ namespace Towerdefense
         SortedList<int, List<Enemy>> waveList;
 
         /*actual wave*/
-        int actualWave;
+        int actualWave = 0;
 
-        public enum enemyType
+        /*LevelObject which contains Content loaded out of an *.xml */
+        LoadLevel levelObject;
+
+        public GameManager()
         {
-            Wolf
-        };
-
-        public GameManager() { }
-
-
-        public GameManager(int waveQuantity, int[] enemyQuantityPerWave, enemyType[] enemyTypeInWave)
-        {
-
-            for (int i = 0; i < waveQuantity; i++)
-            {
-                List<Enemy> temp = new List<Enemy>();
-                for (int j = 0; j < enemyQuantityPerWave.Length; j++)
-                {
-                    temp.Add(makeEnemy(enemyTypeInWave[i]));
-                }
-                waveList.Add(i, temp);
-            }
-        }
-
-        private Enemy makeEnemy(enemyType newEnemyType)
-        {
-            Enemy newEnemy;
-            Texture2D sprite;
-            spriteList.TryGetValue(newEnemyType, out sprite);
-
-            switch (newEnemyType)
-            {
-                case enemyType.Wolf:
-                    newEnemy = new Enemy(sprite, 5, 5, 5.5f, "Eis", false, false);
-                    break;
-                default:
-                    newEnemy = null;
-                    break;
-            }
-            return newEnemy;
+            levelObject = new LoadLevel();
+            levelObject.load("./level_1.xml");
+            waveList = levelObject.getWaves();
         }
 
         public void draw()
--- Towerdefense/Towerdefense/src/loadLevel.cs
+++ Towerdefense/Towerdefense/src/loadLevel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Towerdefense.src
+namespace Towerdefense
 {
     class LoadLevel
     {
