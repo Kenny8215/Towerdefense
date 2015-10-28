c317f8877ed21884935074f4545be0a9386e11e2
Christoph Kaiser <christoph.kaiser22@googlemail.com>
2015-10-28 17:04:48 +0100

    -loadLevel class

---
+++ Towerdefense/Towerdefense/src/loadLevel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Towerdefense.src
+{
+    class LoadLevel
+    {
+        public void load(String str)
+        {
+
+        }
+
+        public int[,] getGrid()
+        {
+            int[,] ret = new int[,];
+
+            return ret;
+        }
+
+        public SortedList<int,List<Enemy>> getWaves()
+        {
+            SortedList<int, List<Enemy>> ret = new SortedList<int, List<Enemy>>();
+
+            return ret;
+
+        }
+    }
+}
